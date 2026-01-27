4a3a9619585bde4afe64bb0742a065365f451064
Luis-Lagunes <143462492+Luis-Lagunes@users.noreply.github.com>
2026-01-27 20:24:30 +0000

    rough draft still displaying 0 for calculation

---
+++ Module2Challenge/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Module2Challenge.Pages;
+
+public class IndexModel : PageModel
+{
+    [BindProperty]
+    public double MilesDriven { get; set; }
+    [BindProperty]
+    public double GallonsUsed { get; set; }
+
+    public double MPG { get; set; }
+    public bool ShowResult { get; set; }
+
+        public void OnPost()
+        {
+            double milesDriven = MilesDriven;
+            double gallonsUsed = GallonsUsed;
+
+            double MPG = milesDriven / gallonsUsed;
+            
+
+
+            ShowResult = true;
+        }
+    }
